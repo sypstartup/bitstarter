6f7275faca6aa2cd20fe4d00260f38ba17dcb1f3
sypstartup <syp.startup@gmail.com>
2013-07-29 12:34:52 +0000

    modified to print the content of index.html

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var string = fs.readFileSync('index.html', 'utf8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
