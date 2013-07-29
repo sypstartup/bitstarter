dd7462f7cadad6bb991e9b4ddcf5fdcc3a6d94ec
sypstartup <syp.startup@gmail.com>
2013-07-29 15:26:51 +0000

    modifed web.js yet again

--- web.js
+++ web.js
@@ -2,11 +2,17 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var filename = './bitstarter/index.html';
+var buf = fs.readFileSync(filename);
+len = buf.write(fs.readFileSync(filename, 'utf8'), 0);
+console.log(buf.toString('utf8', 0, len));
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
